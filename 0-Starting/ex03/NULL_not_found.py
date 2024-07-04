5f38c000292aa934c5ed4e6aed604d432e08867e
Iban Montero Martinez <imontero@c4r8s2.42urduliz.com>
2024-07-04 19:30:28 +0200

    ex03

---
+++ 0-Starting/ex03/NULL_not_found.py
@@ -0,0 +1,9 @@
+def NULL_not_found(object: any) -> int:
+    obj_type = type(object)
+
+    if object is None:
+        print(f"Nothing: {object} {obj_type}")
+    elif isinstance(object, float) and object != object:
+        print(f"Cheese: {object} {obj_type}")
+    elif isinstance(object, str) and len(object) == 0:
+        print(f"Zero: {object} {}")
