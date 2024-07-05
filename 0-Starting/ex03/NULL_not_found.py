1e97014384ca0611fb3769f4887d38e52d18957f
Wolfframio <116112114+Wolfframio@users.noreply.github.com>
2024-07-05 20:53:37 +0200

    03done

--- 0-Starting/ex03/NULL_not_found.py
+++ 0-Starting/ex03/NULL_not_found.py
@@ -6,4 +6,11 @@ def NULL_not_found(object: any) -> int:
     elif isinstance(object, float) and object != object:
         print(f"Cheese: {object} {obj_type}")
     elif isinstance(object, str) and len(object) == 0:
-        print(f"Zero: {object} {}")
+        print(f"Zero: {object} {obj_type}")
+    elif isinstance(object, int) and object == 0:
+        print(f"Empty: {object} {obj_type}")
+    elif isinstance(object, bool) and object == False:
+        print(f"Fake: {object} {obj_type}")
+    else:
+        print("Type not Found")
+        return 1
