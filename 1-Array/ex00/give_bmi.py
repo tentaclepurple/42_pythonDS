2ffb5b1d83001dac0696bba19e5b2accb8456036
Iban Montero Martinez <imontero@c3r6s2.42urduliz.com>
2024-07-10 20:35:09 +0200

    01 y 02

---
+++ 1-Array/ex00/give_bmi.py
@@ -0,0 +1,35 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) -> \
+        list[float]:
+    """
+    Calculate BMI for each person given their height and weight.
+    """
+    try:
+        if len(height) != len(weight):
+            raise ValueError
+
+        return [w / (h ** 2) for h, w in zip(height, weight)]
+    except TypeError:
+        print("Both height and weight should be lists of integers or floats.")
+    except ZeroDivisionError:
+        print("Height must be greater than zero to calculate BMI.")
+    except ValueError:
+        print("Height and weight lists must be of the same length.")
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    Check if the BMI of each person is greater than the given limit.
+    """
+    try:
+        return [b > limit for b in bmi]
+    except TypeError:
+        print("BMI list and limit must be integers or floats.")
+
+
+if __name__ == "__main__":
+    height = [1.75, 1.65, 1.85, 1.55]
+    weight = [70, 55, 80, 45]
+    bmi = give_bmi(height, weight)
+    print(bmi)
+    limit = 25
+    print(apply_limit(bmi, limit))
---
+++ 1-Array/ex01/array2D.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """
+    Given a 2D list, return a new 2D list that
+    is a slice of the original list
+    """
+    try:
+
+        if not isinstance(family, list) or not \
+                all(isinstance(row, list) for row in family):
+            raise ValueError("Input must be a list of lists")
+
+        row_length = len(family[0])
+        print(([len(row) == row_length for row in family]))
+        if not all(len(row) == row_length for row in family):
+            raise ValueError("All rows must have the same length")
+
+        family_array = np.array(family)
+
+        initial_shape = family_array.shape
+        print(f"My shape is : {initial_shape}")
+
+        truncated_family = family_array[start:end]
+
+        new_shape = truncated_family.shape
+        print(f"My new shape is : {new_shape}")
+
+        return truncated_family.tolist()
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
